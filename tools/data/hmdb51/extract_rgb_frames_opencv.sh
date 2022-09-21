90599af6b2e3eb5ecf53b84775a3757737931a60
Lee-daeho <54104389+Lee-daeho@users.noreply.github.com>
2022-09-21 12:45:39 +0900

    Update extract_rgb_frames_opencv.sh

--- tools/data/hmdb51/extract_rgb_frames_opencv.sh
+++ tools/data/hmdb51/extract_rgb_frames_opencv.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd ../
-python build_rawframes.py ../../data/hell/videos/ ../../data/hell/rawframes/ --task rgb --level 2 --ext mp4 --use-opencv
+python build_rawframes.py ../../data/fire/videos/ ../../data/fire/rawframes/ --task rgb --level 2 --ext mp4 --use-opencv
 echo "Genearte raw frames (RGB only)"
 
 cd hmdb51/
